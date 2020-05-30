eb4b0e14d4accb786b5ab027f99833dabc2afb4f
Ray Froehlich <ray.froehlich@plexus.com>
2020-05-30 08:22:10 -0500

    adding some test audio

--- button_processor/Program.cs
+++ button_processor/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.IO.Ports;
+using System.Media;
 
 namespace button_processor
 {
     class Program
     {
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns></returns>
+        SerialPort getSerialPort()
+        {
+            SerialPort sp = new SerialPort();
+            sp.PortName = "COM3";
+            sp.BaudRate = 9600;
+            sp.Parity = Parity.None;
+            sp.DataBits = 8;
+            sp.StopBits = StopBits.One;
+            sp.Handshake = Handshake.None;
+            sp.ReadTimeout = 500;
+            sp.WriteTimeout = 500;
+            return sp;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var startupSoundPlayer = new SoundPlayer("startup.wav");
+            startupSoundPlayer.Play();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
